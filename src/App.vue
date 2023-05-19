11b86c9e6f152a0ec8937662545f20bf29c03899
AlbertoScalia <albscalia@gmail.com>
2023-05-19 12:37:01 +0200

    Feat : Create project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vite World'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+    {{ message }}
+  </h1>
+  
+</template>
+
+<style>
+</style>
